c09bcfe82472e83d8e7b6b74cd01eca663e06273
Pratya0011 <pratya0011@gmail.com>
2023-08-11 16:15:02 +0530

    added project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,10 +5,27 @@ import Untitled from "./Untitled.png";
 function Projects() {
   const react = projects.filter(data=>data.type==='React')
   const vanilla = projects.filter(data=>data.type==='Vanilla')
+  const mern = projects.filter(data=>data.type==='mern')
   return (
     <div className="project-div">
       <h2>Web Devlopment Projcts</h2>
       <h3>WHAT I'V MADE</h3>
+      <h2>MERN</h2>
+      <div className="project-container">
+        {mern.map((projects,index) => (
+          <div className="project2" key={index}>
+          <a href={projects.href} target="_blank">
+            <div>
+              <img src={projects.img} height="140px" width="200px" />
+              <div className="title">{projects.title}</div>
+            </div>
+            <div className="project-about">
+              <div className="pname">{projects.about}</div>
+            </div>
+          </a>
+        </div>
+        ))}
+        </div>
       <h2>React JS & REDUX</h2>
       <div className="project-container">
         {react.map((projects,index) => (
@@ -23,7 +40,6 @@ function Projects() {
             </div>
           </a>
         </div>
-        
         ))}
         </div>
         <h2>Vanilla JS</h2>
